da89014c3fbadc625c5164214973294ab20e7fd4
Logu123 <logu1686@gmail.com>
2017-05-20 12:37:57 +0530

    first commit

---
+++ src/test/java/githubTest/GitHubTest.java
@@ -0,0 +1,28 @@
+package githubTest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GitHubTest {
+	
+	@Test
+	public void googleTest() throws IOException
+	{
+		System.setProperty("webdriver.gecko.driver", "C:\\selenium\\webdriverTest\\folder\\geckodriver.exe");
+		WebDriver wd=new FirefoxDriver();
+		wd.manage().window().maximize();
+		wd.get("https://www.flipkart.com/");
+		File src=((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+		//FileUtils.copyFile(src, new File("F:\\Testing_Materials\\Vmetrics_Selenuim\\SelenuimWorkSpace\\WebDriverTest\\Screenshots\\google.jpg"));
+		FileUtils.copyFile(src, new File("G:\\workspace\\GitHubTest\\ScreenShoot\\Flipkart.jpg"));
+	}
+	
+
+}
